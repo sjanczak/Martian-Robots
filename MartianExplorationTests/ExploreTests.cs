c54fd90adfd7061b0b8aedc4f138c1f40103fb5e
Sean Janczak <sjanczak@hotmail.com>
2024-05-13 17:31:07 +0100

    Fixed incorrect comments for tests where code was copied.

--- MartianExplorationTests/ExploreTests.cs
+++ MartianExplorationTests/ExploreTests.cs
@@ -13,7 +13,7 @@ namespace RobotTests
         // The remaining input consists of a sequence of robot positions and instructions (two lines per
         // robot). A position consists of two integers specifying the initial coordinates of the robot and
         // an orientation (N, S, E, W), all separated by whitespace on one line. A robot instruction is a
-        // string of the letters “L”, “R”, and “F” on one line.
+        // string of the letters ï¿½Lï¿½, ï¿½Rï¿½, and ï¿½Fï¿½ on one line.
         // Each robot is processed sequentially, i.e., finishes executing the robot instructions before the
         // next robot begins execution.
         // The maximum value for any coordinate is 50.
@@ -21,7 +21,7 @@ namespace RobotTests
 
         // The Output
         // For each robot position/instruction in the input, the output should indicate the final grid
-        // position and orientation of the robot. If a robot falls off the edge of the grid the word “LOST”
+        // position and orientation of the robot. If a robot falls off the edge of the grid the word ï¿½LOSTï¿½
         // should be printed after the position and orientation.
 
         // Sample Input
@@ -56,7 +56,7 @@ namespace RobotTests
             string telemetryCommands =
             @"30 30\r\n31 1 E\r\nRFRFRFRF";
 
-            // We should get an expception for this test as the maximum value for any coordinate is 50.
+            // We should get an expception for this test as the robot x coordinate exceeeds the grid.
             new Exploration(telemetryCommands);
         }
 
@@ -67,7 +67,7 @@ namespace RobotTests
             string telemetryCommands =
             @"30 30\r\n30 31 E\r\nRFRFRFRF";
 
-            // We should get an expception for this test as the maximum value for any coordinate is 50.
+            // We should get an expception for this test as the robot y coordinate exceeds the grid.
             new Exploration(telemetryCommands);
         }
 
@@ -78,7 +78,7 @@ namespace RobotTests
             string telemetryCommands =
             @"50 50\r\n1 1 E\r\nRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRFRF";
 
-            // We should get an expception for this test as the maximum value for any coordinate is 50.
+            // We should get an expception for the instaruction cannot exceed 100 characters.
             new Exploration(telemetryCommands);
         }
 
